292a2e6df17f0963189811fa5f3178e2201e8e3e
dobbbo <dobson.adam21@gmail.com>
2022-10-12 15:27:10 +1100

    Lesson finished

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:8080/trello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
 
-print(db.__dict__)
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+
+# Define a custom CLI (terminal) command
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables Created')
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print('Tables dropped')
+
+@app.cli.command('seed')
+def seed_db():
+    card = Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Creating the database',
+        status = 'To Do',
+        priority = 'High',
+        date = date.today()
+    )
+
+    db.session.add(card)
+    db.session.commit()
+    print('Tables seeded')
 
 @app.route('/')
 def index():
